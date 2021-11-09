e0009aaeaaa26c28e7231b818f7432c478ee1295
gaojing <jinggao1023@gmail.com>
2021-11-09 11:09:36 -0500

    Entities&ItemManager: modifications and javadoc

--- src/main/java/Entities/Locker.java
+++ src/main/java/Entities/Locker.java
@@ -3,22 +3,42 @@ package Entities;
 import java.util.Map;
 
 public class Locker implements Container{
-    private Map<String, Boolean> lmap;
-    private final int capacity = 0;
+    /**
+     * The map of this Locker, with index of the locations and its state(empty or not).
+     */
+    private final Map<String, Boolean> lmap;
+    /**
+     * The capacity of this Locker.
+     */
+    private final int capacity;
+    /**
+     * The current number of items in this Locker. Default is 0.
+     */
     private int number_items;
+    /**
+     * The number of vacant spaces of this Locker.
+     */
     private int Vacancy;
 
-    public Locker(int number_items, int vacancy, Map<String, Boolean> lmap){
-        this.number_items = number_items;
-        this.lmap= lmap;
-        this.Vacancy = vacancy;
+    /**
+     * A new, empty locker, with a preset capacity and map.
+     */
+    public Locker(int capacity, Map<String, Boolean> lmap){
+        this.capacity = capacity;
+        this.lmap = lmap;
+        this.Vacancy = capacity;
+        this.number_items = 0;
     }
 
+    /**
+     * Make modifications to the container when an item is added.
+     */
     @Override
-    public void modifyMap(String location){
+    public void modifyContainer(String location){
+        this.number_items = this.number_items + 1;
+        this.Vacancy = this.Vacancy - 1;
         this.lmap.replace(location, false, true);
     }
-
 
     @Override
     public int getCapacity() {
@@ -35,6 +55,9 @@ public class Locker implements Container{
         return this.Vacancy;
     }
 
+    /**
+     * Automatically find the next vacant location for a new item to be placed in.
+     */
     @Override
     public String nextVacantLocation() {
         for (String location: lmap.keySet()){
--- src/main/java/Entities/Refrigerator.java
+++ src/main/java/Entities/Refrigerator.java
@@ -1,22 +1,42 @@
 package Entities;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class Refrigerator implements Container {
-    private Map<String, Boolean> rmap;
-    private final int capacity = 0;
+    /**
+     * The map of this Refrigerator, with index of the locations and its state(empty or not).
+     */
+    private final Map<String, Boolean> rmap;
+    /**
+     * The capacity of this Refrigerator.
+     */
+    private final int capacity;
+    /**
+     * The current number of items in this Refrigerator. Default is 0.
+     */
     private int number_items;
+    /**
+     * The number of vacant spaces of this Refrigerator.
+     */
     private int Vacancy;
 
-    public Refrigerator(int number_items, int vacancy){
-        this.number_items = number_items;
-        this.Vacancy = vacancy;
-        this.rmap = new HashMap<String, Boolean>();
+    /**
+     * A new, empty Refrigerator, with a preset capacity and map.
+     */
+    public Refrigerator(int capacity, Map<String, Boolean> rmap){
+        this.capacity = capacity;
+        this.rmap = rmap;
+        this.Vacancy = capacity;
+        this.number_items = 0;
     }
 
+    /**
+     * Make modifications to the container when an item is added.
+     */
     @Override
-    public void modifyMap(String location){
+    public void modifyContainer(String location) {
+        this.number_items = this.number_items + 1;
+        this.Vacancy = this.Vacancy - 1;
         this.rmap.replace(location, false, true);
     }
 
