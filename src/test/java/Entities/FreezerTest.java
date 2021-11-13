a6f037bd75a70fc7d58f9687c89a48a4364c229c
Waannng <queenie.wang@mail.utoronto.ca>
2021-11-13 01:34:14 -0500

    Entities Test

---
+++ src/test/java/Entities/FreezerTest.java
@@ -0,0 +1,70 @@
+package Entities;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class FreezerTest {
+
+    @Test
+    public void modifyContainer() {
+        Map<String, Boolean> fmap = new HashMap<>(1);
+        fmap.put("L01", false);
+        Locker F = new Locker(1, fmap);
+        F.modifyContainer("L01");
+        assertTrue(F.generateMap().get("L01"));
+    }
+
+    @Test
+    public void getCapacity() {
+        Map<String, Boolean> fmap = new HashMap<>(2);
+        fmap.put("L01", false);
+        fmap.put("L02", false);
+        Locker F = new Locker(2, fmap);
+        assertEquals(2, F.getCapacity());
+    }
+
+    @Test
+    public void getNumberOfItems() {
+        Map<String, Boolean> fmap = new HashMap<>(2);
+        fmap.put("L01", false);
+        fmap.put("L02", false);
+        Locker F = new Locker(2, fmap);
+        assertEquals(0, F.getNumberOfItems());
+        F.modifyContainer("L01");
+        assertEquals(1, F.getNumberOfItems());
+    }
+
+    @Test
+    public void getVacancy() {
+        Map<String, Boolean> fmap = new HashMap<>(2);
+        fmap.put("L01", false);
+        fmap.put("L02", false);
+        Locker F = new Locker(2, fmap);
+        assertEquals(2, F.getVacancy());
+        F.modifyContainer("L01");
+        assertEquals(1, F.getVacancy());
+    }
+
+    @Test
+    public void generateMap() {
+        Map<String, Boolean> fmap = new HashMap<>(1);
+        fmap.put("L01", false);
+        Locker F = new Locker(1, fmap);
+        assertNotNull(F.generateMap());
+    }
+
+    @Test
+    public void nextVacantLocation() {
+        Map<String, Boolean> fmap = new HashMap<>(3);
+        fmap.put("L01", false);
+        fmap.put("L02", true);
+        fmap.put("L03", false);
+        Locker F = new Locker(3, fmap);
+        assertEquals("L01", F.nextVacantLocation());
+        assertEquals("L01", F.nextVacantLocation());
+    }
+}
---
+++ src/test/java/Entities/UserTest.java
@@ -0,0 +1,20 @@
+package Entities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @Test
+    public void getPassword() {
+        User Queenie = new User("queenie", "123456");
+        assertEquals("123456", Queenie.getPassword());
+    }
+
+    @Test
+    public void getUsername() {
+        User Queenie = new User("queenie", "123456");
+        assertEquals("queenie", Queenie.getUsername());
+    }
+}
