6c0e09c0cb2f78399f305eeb45ba7bd7d5795fe6
alanchen34 <alanchen435@gmail.com>
2021-11-15 14:04:16 -0500

    all test fixed

--- src/main/java/Entities/User.java
+++ src/main/java/Entities/User.java
@@ -1,9 +1,20 @@
 package Entities;
 import java.io.*;
 public class User implements Serializable{
+    /**
+     * The string of the username
+     */
     private String username;
+    /**
+     * The string of the user's password
+     */
     private String password;
-    
+
+    /**
+     * create a user of the system
+     * @param username the string of the user's username
+     * @param password the string of the user's password
+     */
     public User(String username, String password){
         this.username = username;
         this.password = password;
