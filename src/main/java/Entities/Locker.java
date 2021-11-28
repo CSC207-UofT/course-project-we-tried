fe634083f80964438f25a90aec666356b30d0eef
wangm203 <mingjiang.wang@mail.utoronto.ca>
2021-11-28 17:19:27 -0500

    Empty storage and bug-fixed version!

--- src/main/java/Entities/Locker.java
+++ src/main/java/Entities/Locker.java
@@ -69,7 +69,7 @@ public class Locker implements Container, Serializable{
         this.number_items = this.number_items - 1;
         this.Vacancy = this.Vacancy + 1;
         this.lmap.replace(location, true, false);
-        FileOutputStream fos = new FileOutputStream("freezer.txt");
+        FileOutputStream fos = new FileOutputStream("locker.txt");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(this.lmap);
     }
