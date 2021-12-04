a42e3f2d12ebec92089a3d30b7a1bf735cbb43d0
QihaoQi <qihao.qi@mail.utoronto.ca>
2021-12-04 17:05:07 -0500

    Closet Vis Final

--- src/main/java/UI/FreezerVisualization.java
+++ src/main/java/UI/FreezerVisualization.java
@@ -14,6 +14,7 @@ public class FreezerVisualization implements ActionListener {
     private ImageIcon LockerImage = new ImageIcon("src/Locker.png");
     private JButton Menu = new JButton("Menu");
     private JButton Search = new JButton("Search");
+    private JButton Container = new JButton("Return");
     private JLabel LockerLocker = new JLabel(LockerImage);
     private ImageIcon VImage = new ImageIcon("src/Vertical.png");
     private ImageIcon HImage = new ImageIcon("src/Horizontal.png");
@@ -107,7 +108,7 @@ public class FreezerVisualization implements ActionListener {
         frame.add(L01);
 
         if (f_list != null) {
-            if (f_list.get(0) != null) {
+            if (f_list.size() > 0) {
                 L1 = new JLabel((String) f_list.get(0));
                 L1.setBounds(110, 63, 200, 100);
                 L1.setFont(new Font(null, Font.PLAIN, 20));
@@ -337,7 +338,16 @@ public class FreezerVisualization implements ActionListener {
         Search.addActionListener(this);
         frame.add(Search);
 
-        Menu.setBounds(850, 500, 200, 100);
+        Container.setBounds(850, 400, 200, 100);
+        Container.setFont(new Font(null, Font.PLAIN, 30));
+        Container.setBorder(BorderFactory.createLineBorder(Color.white));
+        Container.setOpaque(true);
+        Container.setBackground(Color.white);
+        Container.setForeground(Color.darkGray);
+        Container.addActionListener(this);
+        frame.add(Container);
+
+        Menu.setBounds(850, 600, 200, 100);
         Menu.setFont(new Font(null, Font.PLAIN, 30));
         Menu.setBorder(BorderFactory.createLineBorder(Color.white));
         Menu.setForeground(Color.white);
@@ -360,6 +370,16 @@ public class FreezerVisualization implements ActionListener {
             frame.dispose();
             try {
                 MenuPage menuPage = new MenuPage(userID, pickupSystem, loginController);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            } catch (ClassNotFoundException ex) {
+                ex.printStackTrace();
+            }
+        }
+        if (e.getSource() == Container){
+            frame.dispose();
+            try {
+                ContainerMap Cmap = new ContainerMap(userID, pickupSystem, loginController);
             } catch (IOException ex) {
                 ex.printStackTrace();
             } catch (ClassNotFoundException ex) {
